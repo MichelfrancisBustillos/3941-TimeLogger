0adcdc3b9d83b96ea78fb05c134fd907be232b96
0queue <ta.harris.42@gmail.com>
2014-01-15 19:27:46 -0500

    First commit

---
+++ Logger.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+# Logger.py
+# non-gui portions of the time logger
+# Uses sqlite3 as the database
+
+import sqlite3
+import re
+import os
+import time
+
+names = []  # first, last
+cursor = None
+CT = "CREATE TABLE IF NOT EXISTS members(first TEXT, last TEXT, logtime INTEGER, action TEXT)"
+
+class Logger:
+
+    def __init__(self):
+        self.names = []
+        self.conn = None
+        self.c = None # db cursor
+
+        self.initNames()
+        self.connectToDB()
+
+    def initNames(self):
+        tempfname = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "RoboticsNames.csv")
+        namesfile = open(tempfname, "r")
+
+        for line in namesfile.readlines():
+            temp = re.split(",", line)
+            temp[1] = temp[1][:-2] # strip the \r\n
+            self.names.append(tuple(temp))
+
+        self.names = sorted(self.names, key=lambda x: x[1])
+
+        namesfile.close()
+
+    def connectToDB(self):
+        self.conn = sqlite3.connect("people.db")
+        self.c = self.conn.cursor()
+        self.c.execute(CT)
+        self.conn.commit()
+
+    def getInfo(self, first=None, last=None):
+        if first or last:
+            for name in self.names:
+                if name[0] == first or name[1] == last:
+                    return name
+        else:
+            return (None, None)
+    
+    def getLastAction (self, first, last):
+        self.c.execute("SELECT * FROM members WHERE " + \
+                       "first=(?) AND last=(?)", [first, last])
+        self.conn.commit()
+
+        results = sorted(self.c.fetchall(), key=lambda x: x[2])
+        if not results == []:
+            return results[-1][-1]
+        else:
+            return None
+
+    def log(self, first, last, action):
+        
+        t = int(time.time())
+
+        person = self.getInfo(first, last)
+        self.c.execute("INSERT into members VALUES(?,?,?,?)",
+                       [first, last, t, action])
+        self.conn.commit()
+    
+    def logOutAll(self):
+        for name in self.names:
+            if self.getLastAction(name[0], name[1]) == "in":
+                self.log(name[0], name[1], "out")
+
+    def addNewMember(self, first, last):
+        fh = open(os.path.join(os.path.dirname(__file__),
+                  "RoboticsNames.csv"), "a")
+        fh.write(first + "," + last + "\n")
+        fh.close()
+
+if __name__ == "__main__":
+    logger = Logger()
+    logger.addNewUser("NEW", "NAME")
+
---
+++ TLGUI.py
@@ -0,0 +1,183 @@
+#!/usr/bin/python
+# TLGUI.py
+
+# TODO: add menu item to report member totals
+
+import gtk
+import pango
+import Logger
+import os
+import re
+
+class TL:
+    
+    def delete_event(self, w, ev, data=None):
+        return False
+
+    def destroy(self, w, data=None):    
+        gtk.main_quit()
+
+    def updateCombo(self, w, data=None):
+        if not w.get_text() == "":
+            name = tuple(re.split(" ", w.get_text().lower()))
+            for n in self.tldb.names:
+                if re.search("^" + name[0], n[0].lower()):
+                    self.cbox.set_active(self.tldb.names.index(n))
+                elif len(name) == 2 and \
+                     re.search(name[1], n[0].lower()):
+
+                    self.cbox.set_active(self.tldb.names.index(n))
+        else:
+            self.cbox.set_active(-1)
+
+    def button_clicked(self, w, data=None):
+        name = self.tldb.names[self.cbox.get_active()]
+        lastaction = self.tldb.getLastAction(name[0], name[1])
+        if lastaction == "in":
+            self.tldb.log(name[0], name[1], "out")
+        elif lastaction == "out":
+            self.tldb.log(name[0], name[1], "in")
+        else:
+            self.tldb.log(name[0], name[1], "in")
+
+        self.updateLogButton(self.cbox)
+
+    def createCombo(self):
+        self.cbox = gtk.combo_box_new_text()
+
+        cbc = self.cbox.child
+        cbc.modify_font(pango.FontDescription("38"))
+
+        for name in self.tldb.names:
+            self.cbox.append_text(name[0] + " " + name[1])
+
+        self.cbox.connect("changed", self.updateLogButton)
+
+        self.cbhbox = gtk.HBox()
+        self.cbhbox.pack_start(gtk.Label(""))
+        self.cbhbox.pack_start(self.cbox)
+        self.cbhbox.pack_start(gtk.Label(""))
+
+        self.mainvb.pack_start(self.cbhbox, False)
+
+    def createLogButton(self):
+        self.logb = gtk.Button()
+        self.logblbl = gtk.Label("<span size='38000'>Log In</span>")
+        self.logblbl.set_use_markup(True)
+        self.logb.connect("clicked", self.button_clicked)
+        self.logb.add(self.logblbl)
+        self.lbhbox = gtk.HBox()
+        self.lbhbox.pack_start(gtk.Label(""))
+        self.lbhbox.pack_start(self.logb)
+        self.lbhbox.pack_start(gtk.Label(""))
+        self.mainvb.pack_start(self.lbhbox, False)
+
+    def updateLogButton(self, cb, data=None):
+        name = self.tldb.names[cb.get_active()]
+        lastaction = self.tldb.getLastAction(name[0], name[1])
+        if lastaction == "in":
+            self.logblbl.set_text("<span size='38000'>Log Out</span>")
+        elif lastaction == "out":
+            self.logblbl.set_text("<span size='38000'>Log In</span>")
+        else:
+            self.logblbl.set_text("<span size='38000'>Log In</span>")
+        self.logblbl.set_use_markup(True)
+
+    def createMenu(self):
+        self.menubar = gtk.MenuBar()
+        self.mainvb.pack_start(self.menubar)
+
+        self.adminmenu = gtk.Menu()
+        self.adminitem = gtk.MenuItem("Admin")
+        self.adminitem.set_submenu(self.adminmenu)
+        self.addnewmemberitem = gtk.MenuItem("Add New Member")
+        self.addnewmemberitem.connect("activate", self.getNewName)
+        self.logoutallitem = gtk.MenuItem("Log Out All")
+        self.logoutallitem.connect("activate", self.logOutAll)
+        self.adminmenu.append(self.addnewmemberitem)
+        self.adminmenu.append(self.logoutallitem)
+
+        self.menubar.append(self.adminitem)
+
+    def logOutAll(self, w, data=None):
+        self.tldb.logOutAll()
+        message = gtk.MessageDialog(type=gtk.MESSAGE_INFO)
+        message.set_markup("All users logged out.")
+        self.updateLogButton(self.cbox)
+        message.run()
+
+    def getNewName(self, w, data=None):
+        self.newmemberdialog = gtk.Dialog(title="Add New Member",
+                                          flags=gtk.DIALOG_MODAL)
+        label = gtk.Label("Type the new member's name:")
+        self.newmemberdialog.vbox.pack_start(label, True, True, 0)
+
+        hb = gtk.HBox()
+        self.newmemberdialog.vbox.pack_start(hb, True, True, 0)
+
+        self.te = gtk.Entry()
+        hb.pack_start(self.te)
+
+        b = gtk.Button("Add")
+        b.connect("clicked", self.addNewMember)
+        hb.pack_start(b)
+
+        self.newmemberdialog.show_all()
+
+
+    def addNewMember(self, w, nametext=None):
+        # does not work, make sure split okay
+        newname = tuple(re.split(" ", self.te.get_text()))
+        if len(newname) >= 2:
+            self.tldb.addNewMember(newname[0], newname[1])
+        else:
+            self.tldb.addNewMember(newname[0], " ")
+            
+
+        message = gtk.MessageDialog(type=gtk.MESSAGE_INFO)
+        message.set_markup("New user added: " + \
+                           newname[0] + " " + newname[1]) 
+        self.newmemberdialog.destroy()
+        message.run()
+
+    def __init__(self, tldb):
+        self.tldb = tldb
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.set_title("Team 3941 Time Logger")
+        self.window.connect("delete_event", self.destroy)
+
+        self.mainvb = gtk.VBox()
+        self.window.add(self.mainvb)
+
+        self.createMenu()
+
+        self.mainvb.add(gtk.Label(""))
+
+        self.searchhb = gtk.HBox(False, 0)
+        self.mainvb.pack_start(self.searchhb, False)
+        
+        self.searchhb.pack_start(gtk.Label(""))
+        self.label1 = gtk.Label("<span size='38000'>Search: </span>")
+        self.label1.set_use_markup(True)
+        self.searchhb.pack_start(self.label1, False, False, 0)
+
+        self.searchEntry = gtk.Entry(max=0)
+        self.searchEntry.connect("changed", self.updateCombo)
+        self.searchEntry.modify_font(pango.FontDescription("38"))
+        self.searchhb.pack_start(self.searchEntry)
+        self.searchhb.pack_start(gtk.Label(""))
+
+        self.createCombo()
+        self.createLogButton()
+
+        self.mainvb.add(gtk.Label(""))
+
+        self.window.show_all()
+
+    def main(self):
+        gtk.main()
+
+if __name__ == "__main__":
+    tldb = Logger.Logger()
+    tl = TL(tldb)
+    tl.main()
